a06032cded787e3ce8a7dd9e114a685bf42c333a
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-04-02 14:32:57 +0300

    feat(icons): add ManInGray

---
+++ src/components/ui/icons/ManInGray/ManInGray.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './man-in-gray/index.css';
+
+const ManInGray = () => {
+  return (
+    <div className="man-in-gray">
+    </div>
+  );
+}
+
+export default ManInGray;
---
+++ src/components/ui/icons/ManInGray/ManInGray.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ManInGray from './ManInGray';
+
+export default {
+  title: 'Design System/Atoms/Icons/ManInGray',
+  component: ManInGray,
+}
+
+export const Unic = () => <ManInGray />
