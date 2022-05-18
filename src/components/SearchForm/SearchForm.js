f4333c767fab3031ae7169b2afc0e5a5b6c806ef
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-05-18 20:31:51 +0300

    refactor(search-form): lighter borger on focus

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SolidButton from '../ui/buttons/solid/SolidButton';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import { actionButtonText } from '../../shared/constants/buttons/button-text'; 
@@ -6,17 +6,22 @@ import { checkBoxCaptions } from '../../shared/constants/captions/captions';
 import './index.css';
 
 export default function SearchForm() {
+  const [isFormFocused, setFormFocused] = useState(false);
+
   return (
     <section className="search-form">
-      <div className="search-form__form">
+      <div className={`search-form__form ${isFormFocused ? 'search-form__form_focused' : ''}`}>
         <form className="search-form__form-entity" name="search-form">
           <input
             type="text"
+            required
             className="search-form__search-input"
             name="search-input"
             id="search-input"
             placeholder="Фильм"
             size="10"
+            onFocus={() => setFormFocused(true)}
+            onBlur={() => setFormFocused(false)}
           />
           <SolidButton
             view="blue-search"
