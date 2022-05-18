66ba1e2e470f0af0342bcfa87a4f50501b8aa9a7
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-05-18 19:28:11 +0300

    refactor(MoviesSection): remove "more" button

--- src/components/MoviesSection/MoviesSection.js
+++ src/components/MoviesSection/MoviesSection.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { useLocation } from 'react-router-dom';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import GhostButton from '../ui/buttons/ghost/GhostButton'; 
 import { actionButtonText } from '../../shared/constants/buttons/button-text';
@@ -7,26 +8,30 @@ import moviesExplorer from '../../utils/api/MoviesExplorer';
 
 export default function MoviesSection() {
   const [movies, setMovies] = useState([]);
+  let location = useLocation();
 
-  useEffect(() => moviesExplorer.getMovies()
-    .then(allMovies => {
-      let moviesArr = [];
-      for (let i = 0; i < 12; i++ ) {
-        moviesArr.push({
-          id: allMovies[i].id,
-          title: allMovies[i].nameRU,
-          duration: allMovies[i].duration,
-          posterUrl: allMovies[i].image.url,
-        });
-      }
-      setMovies(moviesArr);
+  useEffect(() => {
+    moviesExplorer.getMovies()
+      .then(allMovies => {
+        let moviesArr = [];
+        for (let i = 0; i < 12; i++ ) {
+          moviesArr.push({
+            id: allMovies[i].id,
+            title: allMovies[i].nameRU,
+            duration: allMovies[i].duration,
+            posterUrl: allMovies[i].image.url,
+          });
+        }
+        setMovies(moviesArr);
+      });
+      return undefined;
     }
-  ), []);
+  , []);
   
   return (
     <section className="movies-section">
       <MoviesCardList movies={movies} />
-      { movies.length > 3 &&
+      { movies.length > 3 && location.pathname === '/movies' &&
         <div className="movies-section__more">
           <GhostButton label={actionButtonText.more} />
         </div>
