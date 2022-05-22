a5a6f89ee14f120b24f4130583044ae41b3f6de0
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-05-22 20:48:17 +0300

    feat(signupform): add correct classes

--- src/components/SignUpFieldset/SignUpFieldset.js
+++ src/components/SignUpFieldset/SignUpFieldset.js
@@ -6,12 +6,12 @@ export default function SignUpFieldset({errors}) {
     <fieldset className="signup-fieldset">
       <p className="signup-fieldset__item">
         <label className="signup-fieldset__label" htmlFor="name">Имя</label>
-        <input type="text" className="signup-fieldset__input" name="name" id="name" placeholder="Иван" required></input>
+        <input type="text" className="signup-fieldset__input signup-fieldset__input_error" name="name" id="name" placeholder="Иван" required></input>
         <span className="signup-fieldset__error">{errors.name}</span>
       </p>
       <p className="signup-fieldset__item">
         <label className="signup-fieldset__label" htmlFor="email">E-mail</label>
-        <input type="email" className="signup-fieldset__input" name="email" id="email" placeholder="mail@yandex.ru" required></input>
+        <input type="email" className="signup-fieldset__input signup-fieldset__input_correct" name="email" id="email" placeholder="mail@yandex.ru" required></input>
         <span className="signup-fieldset__error">{errors.email}</span>
       </p>
       <p className="signup-fieldset__item">
