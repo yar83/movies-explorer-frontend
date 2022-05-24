fb448fc3a982bb9f83ba61ec8550489a21242b1f
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-05-24 17:17:24 +0300

    hot-fix(api): set proper paths

--- src/components/MoviesSection/MoviesSection.js
+++ src/components/MoviesSection/MoviesSection.js
@@ -4,7 +4,7 @@ import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import GhostButton from '../ui/buttons/ghost/GhostButton'; 
 import { actionButtonText } from '../../shared/constants/buttons/button-text';
 import './index.css';
-import moviesExplorer from '../../utils/MoviesApi';
+import moviesExplorer from '../../utils/api/MoviesApi';
 
 export default function MoviesSection() {
   const [movies, setMovies] = useState([]);
