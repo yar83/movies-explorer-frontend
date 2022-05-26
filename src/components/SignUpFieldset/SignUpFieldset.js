34744271bfbf560efbac16ca2e9567981dbd2d7b
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-05-26 20:55:31 +0300

    improvement(forms): improve useForm

--- src/components/SignUpFieldset/SignUpFieldset.js
+++ src/components/SignUpFieldset/SignUpFieldset.js
@@ -2,22 +2,23 @@ import React from 'react';
 import './index.css';
 
 export default function SignUpFieldset({handleChange, state}) {
+  console.log(state);
   return (
     <fieldset className="signup-fieldset">
       <p className="signup-fieldset__item">
         <label className="signup-fieldset__label" htmlFor="name">Имя</label>
-        <input type="text" className="signup-fieldset__input signup-fieldset__input_error" name="name" id="name" placeholder="Иван" onChange={handleChange} required></input>
-        <span className="signup-fieldset__error">{state.error}</span>
+        <input type="text" className={`signup-fieldset__input ${state.name.isClear ? '' : state.name.error ? 'signup-fieldset__input_error' : 'signup-fieldset__input_correct'}`} name="name" id="name" placeholder="Иван" onChange={handleChange} required></input>
+        <span className="signup-fieldset__error">{state.name.error}</span>
       </p>
       <p className="signup-fieldset__item">
         <label className="signup-fieldset__label" htmlFor="email">E-mail</label>
-        <input type="email" className="signup-fieldset__input signup-fieldset__input_correct" name="email" id="email" placeholder="mail@yandex.ru" onChange={handleChange} required></input>
-        <span className="signup-fieldset__error">{state.error}</span>
+        <input type="email" className={`signup-fieldset__input ${state.email.isClear ? '' : state.email.error ? 'signup-fieldset__input_error' : 'signup-fieldset__input_correct'}`} name="email" id="email" placeholder="mail@yandex.ru" onChange={handleChange} required></input>
+        <span className="signup-fieldset__error">{state.email.error}</span>
       </p>
       <p className="signup-fieldset__item">
         <label className="signup-fieldset__label" htmlFor="password">Пароль</label>
-        <input type="password" className="signup-fieldset__input" name="password" id="password" required placeholder="••••••" onChange={handleChange}></input>
-        <span className="signup-fieldset__error">{state.error}</span>
+        <input type="password" className={`signup-fieldset__input ${state.password.isClear ? '' : state.password.error ? 'signup-fieldset__input_error' : 'signup-fieldset__input_correct'}`} name="password" id="password" required placeholder="••••••" onChange={handleChange}></input>
+        <span className="signup-fieldset__error">{state.password.error}</span>
       </p>
       <p className="signup-fieldset__general-error">
         Что-то пошло не так...
