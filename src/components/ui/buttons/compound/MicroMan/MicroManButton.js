42823d8abd7473fa46c7ea42ef010e7e5977c557
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-06-08 19:53:15 +0300

    refactor(micro-man-button): add main page button theme

--- src/components/ui/buttons/compound/MicroMan/MicroManButton.js
+++ src/components/ui/buttons/compound/MicroMan/MicroManButton.js
@@ -3,11 +3,11 @@ import './index.css';
 import { moveButtonText } from '../../../../../shared/constants/buttons/button-text';
 import Icon from '../../../icon/Icon';
 
-export default function MicroManButton({handleClick}) {
+export default function MicroManButton({ handleClick, isOnMain }) {
   return (
     <button className="micro-man" onClick={handleClick}>
       {moveButtonText.account}
-      <div className="micro-man__flat">
+      <div className={`micro-man__flat ${isOnMain ? 'micro-man__flat_main' : ''}`}>
         <Icon glyph="micro-man" />
       </div>
     </button>
