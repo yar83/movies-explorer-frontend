adbcef183a15db254f540f491c33a69e2b3b6c52
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-06-08 17:55:01 +0300

    feat(MainHeaderAuthorized): implement authorized header component

--- src/components/MainHeaderAuthorized/MainHeaderAuthorized.js
+++ src/components/MainHeaderAuthorized/MainHeaderAuthorized.js
@@ -2,14 +2,41 @@ import React from 'react';
 import GreenS from '../ui/labels/GreenS/GreenS';
 import TextButton from '../ui/buttons/text/TextButton';
 import MicroManButton from '../ui/buttons/compound/MicroMan/MicroManButton';
+import IconButton from '../ui/buttons/icon/IconButton';
+import { moveButtonText } from '../../shared/constants/buttons/button-text';
 import './index.css';
 
 export default function MainHeaderAuthorized() {
 
   return (
-    <div className="main-header-authorized">
-      <div className="main-header-authorized__wrapper">
+    <header className="main-header-authorized-wrapper">
+      <div className="main-header-authorized">
+        <div className="main-header-authorized__greens">
+          <GreenS/>
+        </div>
+        <nav className="main-header-authorized__nav">
+          <ul className="main-header-authorized__nav-list">
+            <li className="main-header-authorized__nav-item">
+              <TextButton
+                label={moveButtonText.movies}
+                color="white"
+                fontWeight="medium"
+                fontSize="12"
+              />              
+            </li>
+            <li className="main-header-authorized__nav-item">
+              <TextButton
+                label={moveButtonText.savedMovies}
+                color="white"
+                fontSize="12"
+                fontWeight="medium"
+              />
+            </li>
+          </ul>
+        </nav>
+        <MicroManButton />
+        <IconButton glyph="burger" view="burger" />
       </div>
-    </div>
+    </header>
   );
 };
